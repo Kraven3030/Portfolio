714f856616d3d9f63553d2b3c477eaca226eee6c
BlakeR <blakereed3030@gmail.com>
2022-11-04 16:04:04 -0700

    modal complete

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,25 @@
-// Funtion to make celtic spiral play or pause spin
+// Modal elements
 
-// function spin(rotate) {
-//     const spiral = document.getElementById(rotate);
-//     spiral.style.animation = "rotation 7s linear infinite"
+const modal = document.getElementById("theModal");
 
-// }
+const modalBtn = document.getElementById("aboutme_button");
+
+const closeBtn = document.getElementsByClassName("close_button")[0]
+
+// Event listeners to open modal
+modalBtn.addEventListener("click", openModal);
+// Event listener to close modal
+closeBtn.addEventListener("click", closeModal);
+
+// Funtion to open modal
+function openModal() {
+    modal.style.display = "block"
+}
+
+// Function to close modal
+function closeModal() {
+    modal.style.display = "none"
+}
 
 
 
