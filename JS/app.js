82036e76597550ca5ff9373b27c25aa07816ceb9
BlakeR <blakereed3030@gmail.com>
2022-11-04 13:23:54 -0700

    update

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,10 @@
 // Funtion to make celtic spiral play or pause spin 
 
+function spin(rotate) {
+    const spiral = document.getElementById(rotate);
+    spiral.style.animation = "rotation 7s linear infinite"
 
+}
 
 
 
