3bc5e0a59792cefe063bc807cf29912e749fe16c
BlakeR <blakereed3030@gmail.com>
2022-11-04 10:33:17 -0700

    merge

---
+++ Portfolio/app.js
@@ -0,0 +1,8 @@
+// Funtion to make celtic spiral play or pause spin 
+
+
+const spin = document.querySelector("#spiralStop");
+
+
+
+
--- app.js
+++
@@ -1,8 +0,0 @@
-// Funtion to make celtic spiral play or pause spin 
-
-
-const spin = document.querySelector("#spiralStop");
-
-
-
-
